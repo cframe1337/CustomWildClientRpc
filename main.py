43211d8fb824321d0882d6103820aba3c2da4e85
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2024-03-21 14:47:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import os as o
+import sys as s
+import time as t
+
+from colorama import init, Fore
+from pypresence import Presence
+
+init(autoreset=True)
+
+
+def premain(userid: any):
+    assert len(userid) != 0
+    main(userid)
+    o.system("pause")
+
+
+def main(userid: any):
+    rpc.update(
+        state=f"User ID: {userid}",
+        start=round(t.time()),
+        large_image="https://github.com/cframe1337/CustomWildClientRpc/blob/main/wild-icon.gif?raw=true",
+        large_text="Build: March 21, 2024",
+        buttons=[{"label": "Приобрести", "url": "https://wildclient.org/"},
+                 {"label": "ВКонтакте", "url": "https://vk.com/wildclient/"}])
+
+    s.stdout.write(Fore.YELLOW + '\nНажмите Enter чтобы закрыть программу.')
+
+
+def title(text: str):
+    o.system(f'title {text}')
+
+
+if __name__ == '__main__':
+    rpc = Presence(client_id=1209164668015218718)
+    # Здесь вы можете использовать свой клиент айди скопированный в проекте на Discord Developers Portal
+    # You can use own client id that you copied from project at Discord Developers Portal
+    rpc.connect()
+
+    s.stdout.write(Fore.RED + 'Input your uid' + Fore.YELLOW + ':' + Fore.WHITE + ' ')
+    uid = input()
+    premain(uid)
