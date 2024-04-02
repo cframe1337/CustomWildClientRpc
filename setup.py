66625cf364bdbe3dfa615c7d6ab51ae6d431f7ff
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2024-04-02 20:58:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 import os
 
-with open('requirements.txt') as r:
-    for _ in r:
-        os.system('pip install %s' % _)
+def install_requirements():
+    with open('requirements.txt') as r:
+        for _ in r:
+            os.system('pip install %s' % _)
+
+def start():
+    os.system('python3 main.py')
+    
+
+if __name__ == '__main__':
+    install_requirements()
+    start()
