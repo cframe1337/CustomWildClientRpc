3d26a1ff3c5211b5ff48450e098282a02cfb1372
cframe1337 <110549027+cframe1337@users.noreply.github.com>
2024-10-13 10:53:33 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 pyinstaller main.py -n "CustomWildClientRPC" --onefile
 
-:: Для преобразования вашего скрипта в exe программу Вы можете использовать Pyinstaller
 :: To change python scripts in exe programs you can use Pyinstaller
 :: pip install pyinstaller
 :: (python from 3.8 to 3.12)
